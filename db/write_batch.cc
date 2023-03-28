e21e55c0d2f72af37adf1dae07114d717e9cd304
JohnsonLee <0xjohnsonlee@gmail.com>
2023-03-28 20:08:27 +0800

    WIP

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -9,6 +9,7 @@
 // record :=
 //    kTypeValue varstring varstring         |
 //    kTypeDeletion varstring
+//    kTypeFence varstring varint32
 // varstring :=
 //    len: varint32
 //    data: uint8[len]
@@ -17,7 +18,9 @@
 
 #include "db/dbformat.h"
 #include "db/memtable.h"
+#include "db/version_edit.h"
 #include "db/write_batch_internal.h"
+#include "db/murmurhash3.h"
 #include "leveldb/db.h"
 #include "util/coding.h"
 
@@ -68,6 +71,15 @@ Status WriteBatch::Iterate(Handler* handler) const {
           return Status::Corruption("bad WriteBatch Delete");
         }
         break;
+      case kTypeFence: {
+        uint32_t level;
+        if(GetLengthPrefixedSlice(&input, &key) && GetVarint32(&input, &level)) {
+          handler->HandleFence(key, level);
+        }else {
+          return Status::Corruption("unknown WriteBatch tag"); 
+        }
+      }
+        break;
       default:
         return Status::Corruption("unknown WriteBatch tag");
     }
@@ -102,6 +114,13 @@ void WriteBatch::Put(const Slice& key, const Slice& value) {
   PutLengthPrefixedSlice(&rep_, value);
 }
 
+void WriteBatch::PutFence(const Slice& key, int level){
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
+  rep_.push_back(static_cast<char>(kTypeFence));
+  PutLengthPrefixedSlice(&rep_, key);
+  PutVarint32(&rep_, level);
+}
+
 void WriteBatch::Delete(const Slice& key) {
   WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
   rep_.push_back(static_cast<char>(kTypeDeletion));
@@ -126,6 +145,101 @@ class MemTableInserter : public WriteBatch::Handler {
     mem_->Add(sequence_, kTypeDeletion, key, Slice());
     sequence_++;
   }
+  void HandleFence(const Slice& key, unsigned level) override{
+    assert(level < config::kNumLevels);
+    FenceMetaData* f = new FenceMetaData();
+    // TODO: Determine the type
+    InternalKey ikey(key, sequence_, kTypeValue);
+    f->fence_key = ikey;
+    f->level = level;
+    f->number_segments = 0;
+    f->refs = 1;
+    sequence_++;
+  }
+};
+
+/* 
+  class to iterate over WriteBatch and check if a key should be
+  a fence. If so, do two things: 
+  - add to the string representation (this will later be appended to the
+  WriteBatch contents.
+  - insert it into the Versions structure. 
+*/
+ class GuardInserter : public WriteBatch::Handler {
+ public:
+   GuardInserter() : sequence_(), bit_mask(0) {
+     new_batch = NULL;
+     for (int i = 0; i < config::kNumLevels; i++)
+       num_guards[i] = 0;
+   }
+   WriteBatch* new_batch;
+   SequenceNumber sequence_;
+
+  virtual void Put(const Slice& key, const Slice& value) {
+    // Need to hash and check the last few bits. 
+    void* input = (void*) key.data();
+    size_t size = key.size();
+    const unsigned int murmur_seed = 42;
+    unsigned int hash_result;
+    MurmurHash3_x86_32(input, size, murmur_seed, &hash_result);
+
+    // Go through each level, starting from the top and checking if it
+    // is a guard on that level. 
+    unsigned num_bits = top_level_bits;
+    
+    for (unsigned i = 0; i < config::kNumLevels; i++) {
+      set_mask(num_bits);
+      if ((hash_result & bit_mask) == bit_mask) {
+		// found a guard
+		// Insert the guard to this level and all the lower levels
+		for (unsigned j = i; j < config::kNumLevels; j++) {
+			new_batch->PutFence(key, j);
+			num_guards[j]++;
+		}
+		break;
+      }
+      // Check next level
+      num_bits -= bit_decrement;
+    }
+    sequence_++;
+  }
+
+  virtual void Delete(const Slice& key) {
+    sequence_++;
+  }
+
+  virtual void HandleGuard(const Slice& key, unsigned level) {
+    /* vijayc: emptyHandleGuard. */
+    assert(0);
+  }
+   
+  void print_all_levels() {
+    for (int i = 0; i < config::kNumLevels; i++)
+      if (num_guards[i] > 0)
+	printf("%d %d\n", i, num_guards[i]);
+  }
+   
+ private:
+   // Number of random bits to match in hash value for key to become
+   // top level guard. Note that this has the least probability, will
+   // increase as the levels become deeper.
+   const static unsigned top_level_bits = 27;
+   // Top level guard = 17 bits should match in guard
+   // Next level guard = 15 bits
+   // Next level guard = 13 bits and so on..
+   const static int bit_decrement = 2;
+
+   int num_guards[config::kNumLevels];
+   
+   unsigned bit_mask;
+   
+   void set_mask(unsigned num_bits) {
+     assert(num_bits > 0 && num_bits < 32);
+     bit_mask = (1 << num_bits) - 1;
+   }
+   
+  GuardInserter(const GuardInserter&);
+  GuardInserter& operator = (const GuardInserter&);
 };
 }  // namespace
 
